373fb8593eb3bf4d4d7cd77e168ca7a80acd029c
Kristiankaspersen <kristiankasp@gmail.com>
2022-10-24 17:32:34 +0200

    have added test for success message

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -17,45 +17,80 @@ it('should display the error message "enter all fields"', () => {
 
 })
 
+it('should display the success message"', () => {
+  render(<App />)
+
+  expect(screen.getByLabelText(/username/i)).toHaveValue("")
+  fireEvent.change(screen.getByLabelText(/username/i), {target: {value: 'dog'}})
+  expect(screen.getByLabelText(/username/i)).toHaveValue("dog")
+
+  expect(screen.getByLabelText(/email/i)).toHaveValue("")
+  fireEvent.change(screen.getByLabelText(/email/i), {target: {value: 'dog@dog'}})
+  expect(screen.getByLabelText(/email/i)).toHaveValue("dog@dog")
+
+  expect(screen.getByLabelText(/forename/i)).toHaveValue("")
+  fireEvent.change(screen.getByLabelText(/forename/i), {target: {value: 'test'}})
+  expect(screen.getByLabelText(/forename/i)).toHaveValue("test")
+
+  expect(screen.getByLabelText(/surname/i)).toHaveValue("")
+  fireEvent.change(screen.getByLabelText(/surname/i), {target: {value: 'testesen'}})
+  expect(screen.getByLabelText(/surname/i)).toHaveValue("testesen")
+
+  expect(document.getElementById('password')).toHaveValue("")
+  fireEvent.change(document.getElementById("password"), {target: {value: 'password'}})
+  expect(document.getElementById('password')).toHaveValue("password")
+
+  expect(document.getElementById('repeatPassword')).toHaveValue("")
+  fireEvent.change(document.getElementById('repeatPassword'), {target: {value: 'password'}})
+  expect(document.getElementById('repeatPassword')).toHaveValue("password")
+  
+
+
+  fireEvent.click(screen.getByText("submit"))
+  expect(screen.getByText(/You have successfully registered dog/i)).toHaveTextContent("You have successfully registered dog")
+
+})
+
+
 //Sjekker om formen er der
-test('renders the form', () => {
-  render(<App />);
-  const formElement = document.getElementById("register")
-  expect(formElement).toBeInTheDocument();
-});
+// test('renders the form', () => {
+//   render(<App />);
+//   const formElement = document.getElementById("register")
+//   expect(formElement).toBeInTheDocument();
+// });
 
-test("renders username input", () => {
-  render(<App />);
-  const inputElement = document.getElementById("username")
-  expect(inputElement).toBeInTheDocument();
- }); 
+// test("renders username input", () => {
+//   render(<App />);
+//   const inputElement = document.getElementById("username")
+//   expect(inputElement).toBeInTheDocument();
+//  }); 
 
- test("renders email input", () => {
-  render(<App />);
-  const inputElement = document.getElementById("email")
-  expect(inputElement).toBeInTheDocument();
- }); 
+//  test("renders email input", () => {
+//   render(<App />);
+//   const inputElement = document.getElementById("email")
+//   expect(inputElement).toBeInTheDocument();
+//  }); 
 
- test("renders forename input", () => {
-  render(<App />);
-  const inputElement = document.getElementById("forename")
-  expect(inputElement).toBeInTheDocument();
- }); 
+//  test("renders forename input", () => {
+//   render(<App />);
+//   const inputElement = document.getElementById("forename")
+//   expect(inputElement).toBeInTheDocument();
+//  }); 
 
- test("renders surname input", () => {
-  render(<App />);
-  const inputElement = document.getElementById("surname")
-  expect(inputElement).toBeInTheDocument();
- }); 
+//  test("renders surname input", () => {
+//   render(<App />);
+//   const inputElement = document.getElementById("surname")
+//   expect(inputElement).toBeInTheDocument();
+//  }); 
 
- test("renders password input field", () => {
-  render(<App />);
-  const inputElement = document.getElementById("password")
-  expect(inputElement).toBeInTheDocument();
- }); 
+//  test("renders password input field", () => {
+//   render(<App />);
+//   const inputElement = document.getElementById("password")
+//   expect(inputElement).toBeInTheDocument();
+//  }); 
 
- test("renders repeatPassword input field", () => {
-  render(<App />);
-  const inputElement = document.getElementById("repeatPassword")
-  expect(inputElement).toBeInTheDocument();
- }); 
+//  test("renders repeatPassword input field", () => {
+//   render(<App />);
+//   const inputElement = document.getElementById("repeatPassword")
+//   expect(inputElement).toBeInTheDocument();
+//  }); 
