e8953d36158f07d40210801edae6cd63b0063513
Kristiankaspersen <kristiankasp@gmail.com>
2022-10-24 17:52:25 +0200

    uncommentet some tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -88,44 +88,44 @@ it("should display don't match", () => {
 
 
 //Sjekker om formen er der
-// test('renders the form', () => {
-//   render(<App />);
-//   const formElement = document.getElementById("register")
-//   expect(formElement).toBeInTheDocument();
-// });
+test('renders the form', () => {
+  render(<App />);
+  const formElement = document.getElementById("register")
+  expect(formElement).toBeInTheDocument();
+});
 
-// test("renders username input", () => {
-//   render(<App />);
-//   const inputElement = document.getElementById("username")
-//   expect(inputElement).toBeInTheDocument();
-//  }); 
+test("renders username input", () => {
+  render(<App />);
+  const inputElement = document.getElementById("username")
+  expect(inputElement).toBeInTheDocument();
+ }); 
 
-//  test("renders email input", () => {
-//   render(<App />);
-//   const inputElement = document.getElementById("email")
-//   expect(inputElement).toBeInTheDocument();
-//  }); 
+ test("renders email input", () => {
+  render(<App />);
+  const inputElement = document.getElementById("email")
+  expect(inputElement).toBeInTheDocument();
+ }); 
 
-//  test("renders forename input", () => {
-//   render(<App />);
-//   const inputElement = document.getElementById("forename")
-//   expect(inputElement).toBeInTheDocument();
-//  }); 
+ test("renders forename input", () => {
+  render(<App />);
+  const inputElement = document.getElementById("forename")
+  expect(inputElement).toBeInTheDocument();
+ }); 
 
-//  test("renders surname input", () => {
-//   render(<App />);
-//   const inputElement = document.getElementById("surname")
-//   expect(inputElement).toBeInTheDocument();
-//  }); 
+ test("renders surname input", () => {
+  render(<App />);
+  const inputElement = document.getElementById("surname")
+  expect(inputElement).toBeInTheDocument();
+ }); 
 
-//  test("renders password input field", () => {
-//   render(<App />);
-//   const inputElement = document.getElementById("password")
-//   expect(inputElement).toBeInTheDocument();
-//  }); 
+ test("renders password input field", () => {
+  render(<App />);
+  const inputElement = document.getElementById("password")
+  expect(inputElement).toBeInTheDocument();
+ }); 
 
-//  test("renders repeatPassword input field", () => {
-//   render(<App />);
-//   const inputElement = document.getElementById("repeatPassword")
-//   expect(inputElement).toBeInTheDocument();
-//  }); 
+ test("renders repeatPassword input field", () => {
+  render(<App />);
+  const inputElement = document.getElementById("repeatPassword")
+  expect(inputElement).toBeInTheDocument();
+ }); 
